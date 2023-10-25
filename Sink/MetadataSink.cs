008cbe1c8d01dd8220c0c2cf3f380f26b6a1385c
unknown <yrezehi@gmail.com>
2023-10-25 16:42:37 -0700

    setup: MetadataSink

---
+++ Sink/MetadataSink.cs
@@ -0,0 +1,16 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog_Sink_Metadata.Sink
+{
+    public class MetadataSink : ILogEventSink
+    {
+        private readonly IFormatProvider FormatProvider;
+
+        public MetadataSink(IFormatProvider formatProvider) =>
+            FormatProvider = formatProvider;
+
+        public void Emit(LogEvent logEvent) =>
+            logEvent.RenderMessage(FormatProvider);
+    }
+}
